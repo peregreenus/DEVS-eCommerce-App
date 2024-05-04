af4730d1f09b1adbb8569a4825b453fbc174cb94
Andrewz256 <punkz666@gmail.com>
2024-05-04 11:17:53 +0300

    Chore/set-up-eslint-rss-ecomm-1_10
    * chore: add eslint, eslint config and script for lint to webpack config
    
    * chore: add eslint, eslint config
    
    * chore: add no-explicit-any to eslint config
    
    * chore: add linebreak-style, quotes, semi and coma-dangle rules to eslint config
    
    * chore: add airbnb rules to eslint config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,54 +1,56 @@
-import path from "path";
-import { Configuration } from "webpack";
-import CopyWebpackPlugin from "copy-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import "webpack-dev-server";
+import path from 'path';
+import { Configuration } from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import EslingPlugin from 'eslint-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import 'webpack-dev-server';
 
 const config: Configuration = {
-  mode: (process.env.NODE_ENV as "production" | "development") ?? "development",
-  entry: { app: "./src/index.tsx" },
+  mode: (process.env.NODE_ENV as 'production' | 'development') ?? 'development',
+  entry: { app: './src/index.tsx' },
 
   module: {
     rules: [
       {
         test: /.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.(sc|sa|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    compareBeforeEmit: true,
+    compareBeforeEmit: true
   },
   devServer: {
     open: true,
     hot: true,
     port: 8080,
-    static: path.join(__dirname, "public"),
+    static: path.join(__dirname, 'public')
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html'
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./src/assets/", to: "./assets/" }],
+      patterns: [{ from: './src/assets/', to: './assets/' }]
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css'
     }),
-  ],
+    new EslingPlugin({ extensions: ['ts', '.tsx'] })
+  ]
 };
 
 export default config;
