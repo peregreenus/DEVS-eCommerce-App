b74abe4c049a2d688d29f45869f454d156933622
Andrewz256 <punkz666@gmail.com>
2024-05-05 00:51:50 +0300

    chore/add-jest-with-config-1_13

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src/view/tests/'],
+  testMatch: ['**/*.+(ts|tsx)']
+};
+
+export default config;
