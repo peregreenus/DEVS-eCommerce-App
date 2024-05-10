77a064c4a5f1f4116d736a1ce0cd2bfbba2b26c8
Zelenskiy <Zelensk1973@gmail.com>
2024-05-10 06:21:34 +0300

    feat: add logo to header (#69)

--- src/view/components/common/header/header.tsx
+++ src/view/components/common/header/header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../navbar/navbar';
 import './header.css';
 
 function Header() {
   return (
     <header className="header">
+      <Link className="logo" to="/main">
+        {' '}
+      </Link>
       <Navbar />
     </header>
   );
