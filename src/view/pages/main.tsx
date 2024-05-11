69f270f0691fc7e7e580b38764a84c26886e60df
Zelenskiyas <zelensk1973@gmail.com>
2024-05-11 06:05:29 +0300

    feat: add sections on main page

--- src/view/pages/main.tsx
+++ src/view/pages/main.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import Header from '../components/common/header/header';
 
 function Main() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <section className="greeting main-section">greeting</section>
+      <section className="offer main-section">offer</section>
+      <section className="call-to-action main-section">call-to-action</section>
+    </>
+  );
 }
 
 export default Main;
