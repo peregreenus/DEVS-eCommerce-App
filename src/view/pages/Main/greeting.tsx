dfe71ebcdafd50aee79c456d7b674e7327830ccb
Zelenskiyas <zelensk1973@gmail.com>
2024-05-15 07:37:06 +0300

    refactor: make module style #3

--- src/view/pages/Main/greeting.tsx
+++ src/view/pages/Main/greeting.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import MyButton from '../../components/common/Button/MyButton';
+import * as classes from './greeting.module.css';
 
 function Greeting() {
   const navigate = useNavigate();
@@ -10,10 +11,10 @@ function Greeting() {
   };
 
   return (
-    <section className="greeting main-section">
-      <div className="text-content">
-        <p className="title">Welcome to &quot;Galactic Exclusive&quot;</p>
-        <p className="subtitle">
+    <section className={`${classes.greeting} ${classes.mainSection}`}>
+      <div className={classes.textContent}>
+        <p className={classes.title}>Welcome to &quot;Galactic Exclusive&quot;</p>
+        <p className={classes.subtitle}>
           &quot;Galactic Exclusive&quot; – your ultimate destination for premium real estate in the
           universe! We offer the most exclusive properties in the galaxy for the most discerning
           clients. From luxurious islands to futuristic asteroids – you&apos;ll find everything for
