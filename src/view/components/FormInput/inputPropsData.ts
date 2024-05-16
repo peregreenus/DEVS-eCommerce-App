36b1f760e45e7041b02a55b937fa50f0848b9079
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-16 13:03:04 +0300

    feat: add inputPropsArr

---
+++ src/view/components/FormInput/inputPropsData.ts
@@ -0,0 +1,16 @@
+const inputPropsArr = [
+  {
+    name: 'email',
+    labelName: 'Email',
+    type: 'text',
+    placeholder: 'Enter Your Email'
+  },
+  {
+    name: 'password',
+    labelName: 'Password',
+    type: 'password',
+    placeholder: 'Enter Your Password'
+  }
+];
+
+export default inputPropsArr;
