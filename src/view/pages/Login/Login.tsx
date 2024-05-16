6e8589462946ee5a0e58237126c7aff43fb63081
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-16 13:06:49 +0300

    refactor: change Login page styles

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { MainProps } from '../../../data/types/main-props';
 import Header from '../../components/common/header/header';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import * as classes from './Login.module.css';
 
 function Login({ showMsg, setShowMsg }: MainProps) {
-  // eslint-disable-next-line no-console
-  console.log(classes);
   return (
     <>
       <Header showMsg={showMsg} setShowMsg={setShowMsg} />
       <div className={classes.loginPage}>
         <h2>Login</h2>
+        <LoginForm />
       </div>
     </>
   );
