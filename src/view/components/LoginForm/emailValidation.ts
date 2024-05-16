7363ac5fd96142b9a0618f8b0560381eed067ff5
Aliaksandr Andreichuk <70761678+peregreenus@users.noreply.github.com>
2024-05-16 20:35:48 +0300

    feat: add conditions creating error messages (#83)

--- src/view/components/LoginForm/emailValidation.ts
+++ src/view/components/LoginForm/emailValidation.ts
@@ -3,7 +3,14 @@ function createEmailErrorMessage(value: string) {
 
   let message = '';
 
-  if (emailPattern.test(value)) message = ' ';
+  if (emailPattern.test(value)) {
+    message = ' ';
+  } else if (/^\s+|\s+$/.test(value)) {
+    message = 'Must not contain leading or trailing whitespace.';
+  } else {
+    message =
+      'Must contain an "@" symbol separating local part and domain name (e.g., user@example.com).';
+  }
 
   return message;
 }
