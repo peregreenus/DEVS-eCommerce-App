388960a5874726daedee6ce8eeab0f539f1a578b
Zelenskiyas <zelensk1973@gmail.com>
2024-05-18 19:43:06 +0300

    feat: add burger menu

--- src/view/components/common/navbar/navbar.tsx
+++ src/view/components/common/navbar/navbar.tsx
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import * as classes from './navbar.module.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const burgerRef = useRef<HTMLDivElement>(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && burgerRef.current) {
+      if (
+        !menuRef.current.contains(event.target as Node) &&
+        !burgerRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <nav className={classes.navbar}>
-      <Link to="/">Main</Link>
-      <Link to="/catalog">Catalog</Link>
-      <Link to="/about">About</Link>
-      <Link to="/signup">Register</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/basket">
-        <img
-          className={classes.basketImg}
-          src="../../../../assets/icon/free-icon-shopping-cart-481384.svg"
-          alt="basket"
-        />
-      </Link>
+      <div
+        ref={burgerRef}
+        className={`${classes.burger} ${isOpen ? classes.rotate : ''}`}
+        onClick={toggleMenu}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            toggleMenu();
+          }
+        }}
+        role="button"
+        tabIndex={0}
+        aria-label="Toggle menu">
+        <div className={classes.burgerBar} />
+        <div className={classes.burgerBar} />
+        <div className={classes.burgerBar} />
+      </div>
+      <div ref={menuRef} className={`${classes.menu} ${isOpen ? classes.show : ''}`}>
+        <Link to="/" onClick={toggleMenu}>
+          Main
+        </Link>
+        <Link to="/catalog" onClick={toggleMenu}>
+          Catalog
+        </Link>
+        <Link to="/about" onClick={toggleMenu}>
+          About
+        </Link>
+        <Link to="/signup" onClick={toggleMenu}>
+          Register
+        </Link>
+        <Link to="/login" onClick={toggleMenu}>
+          Login
+        </Link>
+        <Link to="/basket" onClick={toggleMenu}>
+          <img
+            className={classes.basketImg}
+            src="../../../../assets/icon/free-icon-shopping-cart-481384.svg"
+            alt="basket"
+          />
+        </Link>
+      </div>
     </nav>
   );
 }
