439dff827c11cc14181ea4628a10dac98e0c1e6d
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-20 01:18:00 +0300

    feat: add enum CTP

---
+++ src/data/types/ctp.ts
@@ -0,0 +1,11 @@
+enum CTP {
+  PROJECT_KEY = 'devs-ecommerce-app-key-12435687',
+  CLIENT_ID = 'O-WynbMF96PvFryYU0NYLM5R',
+  CLIENT_SECRET = '9a_NIy80rtVZy1sUvscG0Ku6rryPet7Y',
+  ANON_CLIENT_ID = 'vPV0ODXdobttEQAPSAVuaa4Q',
+  ANON_CLIENT_SECRET = 'vdhQd2D4jZpLHXaW2P6dac9kbWMwmVMR',
+  AUTH_URL = 'https://auth.us-central1.gcp.commercetools.com/',
+  API_URL = 'https://api.us-central1.gcp.commercetools.com/'
+}
+
+export default CTP;
