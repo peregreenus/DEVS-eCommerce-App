e007ba7f226213be574dfd810e3a6d912743a03c
Zelenskiyas <zelensk1973@gmail.com>
2024-05-19 12:43:08 +0300

    refactor: remove useRef

--- src/view/components/common/navbar/navbar.tsx
+++ src/view/components/common/navbar/navbar.tsx
@@ -1,50 +1,25 @@
-import React, { useState, useEffect, useRef, KeyboardEvent } from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 import { Link } from 'react-router-dom';
 import * as classes from './navbar.module.css';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const burgerRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   const toggleMenuFromKey = (e: KeyboardEvent) => {
-    if (e.key === 'Enter' || e.key === ' ') {
+    if (e.key === 'Enter' || e.key === ' ' || e.key === 'Escape') {
       e.preventDefault();
       toggleMenu();
     }
   };
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && burgerRef.current) {
-      if (
-        !menuRef.current.contains(event.target as Node) &&
-        !burgerRef.current.contains(event.target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [isOpen]);
-
   return (
     <nav className={classes.navbar}>
       <div
-        ref={burgerRef}
+        // ref={burgerRef}
         className={`${classes.burger} ${isOpen ? classes.rotate : ''}`}
         onClick={toggleMenu}
         onKeyDown={toggleMenuFromKey}
@@ -55,7 +30,7 @@ function Navbar() {
         <div className={classes.burgerBar} />
         <div className={classes.burgerBar} />
       </div>
-      <div ref={menuRef} className={`${classes.menu} ${isOpen ? classes.show : ''}`}>
+      <div className={`${classes.menu} ${isOpen ? classes.show : ''}`}>
         <Link to="/" onClick={toggleMenu}>
           Main
         </Link>
@@ -81,6 +56,18 @@ function Navbar() {
           Cart
         </Link>
       </div>
+      {isOpen ? (
+        <div
+          className={classes.shadow}
+          onClick={toggleMenu}
+          onKeyDown={toggleMenuFromKey}
+          role="button"
+          tabIndex={0}
+          aria-label="Toggle menu"
+        />
+      ) : (
+        <div />
+      )}
     </nav>
   );
 }
