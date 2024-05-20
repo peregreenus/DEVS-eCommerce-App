ce1bbbd6e4e282e86ba8a409b27ef280f0c52925
Zelenskiy <Zelensk1973@gmail.com>
2024-05-20 20:30:47 +0300

    Feat/implement routing for navigation between pages without any errors rss ecomm 2 18 19 (#98)
    * feat: add favicon
    
    * feat: remove div in html according to the technical task
    
    * feat: remove div in html according to the technical task #2

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const div: HTMLElement = document.createElement('div');
+div.setAttribute('id', 'root');
+document.body.append(div);
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
