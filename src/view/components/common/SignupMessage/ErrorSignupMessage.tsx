6155deb96bdba91b29f321b4a7c566332be9c58a
Andrewz256 <punkz666@gmail.com>
2024-05-20 07:30:45 +0300

    feat: add buttons to signup error

--- src/view/components/common/SignupMessage/ErrorSignupMessage.tsx
+++ src/view/components/common/SignupMessage/ErrorSignupMessage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as styles from './SignupMessage.module.css';
 import { SignupProps } from '../../../../data/types/signup-props';
 
@@ -8,17 +9,25 @@ export function setErrorMessage(message: string) {
 }
 
 export default function ErrorSignupMessage({ showSignupState, setShowSignupState }: SignupProps) {
-  const onclickLink = () => {
+  const OkClick = () => {
+    setShowSignupState((prevState) => ({ ...prevState, showSignupError: false }));
+  };
+  const loginClick = () => {
     setShowSignupState((prevState) => ({ ...prevState, showSignupError: false }));
   };
   return (
     <div className={`${showSignupState.showSignupError ? styles.successLink : ''}`}>
       <p>{signupErrorMessage.message}</p>
-      <p>
-        <button type="button" onClick={onclickLink}>
-          OK
+      <div className={styles.buttonContainer}>
+        <button type="button" className={styles.button} onClick={OkClick}>
+          Change Form
         </button>
-      </p>
+        <Link to="/login">
+          <button type="button" className={styles.button} onClick={loginClick}>
+            Login
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
