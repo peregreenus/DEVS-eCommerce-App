3be611ef4a489f1991e6eb41c3b29745f2072fe1
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-20 22:56:16 +0300

    feat: add auto navigate to main page after reload

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import Header from '../../components/common/header/header';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import * as classes from './Login.module.css';
 import { MainProps } from '../../../data/types/main-props';
 
 function Login({ state, setState }: MainProps) {
-  return (
+  const token = localStorage.getItem('bearerToken');
+
+  return token ? (
+    <Navigate to="/" />
+  ) : (
     <>
       <Header state={state} setState={setState} />
       <div className={classes.loginPage}>
