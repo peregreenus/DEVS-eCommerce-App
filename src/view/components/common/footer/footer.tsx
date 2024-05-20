78f8828fb2ef414e2bf02e89ca1d466e2210dbb7
Zelenskiy <Zelensk1973@gmail.com>
2024-05-20 14:58:15 +0300

    feat: links in footer

--- src/view/components/common/footer/footer.tsx
+++ src/view/components/common/footer/footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as classes from './footer.module.css';
 
 function Footer() {
@@ -9,16 +10,27 @@ function Footer() {
       </div>
       <div>
         <p>Contact Information:</p>
-        <p>Phone: +1 (800) 123-4567</p>
-        <p>Email: info@galacticexclusive.com</p>
+        <Link to="tel:+90000000">
+          <p>Phone: +1 (800) 123-4567</p>
+          <p>Email: info@galacticexclusive.com</p>
+        </Link>
       </div>
       <div className={classes.footerItem}>
         <p>Address:</p>
-        <p>123 Space Avenue,</p>
-        <p>Galactic City, GC 00000</p>
+        <Link to="https://www.google.com/maps/@57.8202848,-42.6579508,9.75z?authuser=0&entry=ttu">
+          <p>123 Space Avenue,</p>
+          <p>Galactic City, GC 00000</p>
+        </Link>
       </div>
-      <div className={classes.footerItem}>
+      <div className={`${classes.footerItem} ${classes.iconWrapper}`}>
         <p>Follow us on social media:</p>
+
+        <Link to="https://x.com/">
+          <div className={`${classes.item} ${classes.twitterIcon}`} />
+        </Link>
+        <Link to="https://www.facebook.com/">
+          <div className={`${classes.item} ${classes.facebookIcon}`} />
+        </Link>
       </div>
     </footer>
   );
