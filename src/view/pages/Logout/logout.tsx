a1c534ff41a246628b86fdafe759f59cb160a891
Zelenskiyas <zelensk1973@gmail.com>
2024-05-21 22:17:51 +0300

    feat: add sensitivity after login, no after signup

--- src/view/pages/Logout/logout.tsx
+++ src/view/pages/Logout/logout.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { MainProps } from '../../../data/types/main-props';
 
 function Logout({ setState }: MainProps) {
   localStorage.removeItem('bearerToken');
   localStorage.removeItem('cart');
-  setState((prevState) => ({ ...prevState, userLoggedIn: false }));
+  useEffect(() => {
+    setState((prevState) => ({ ...prevState, userLoggedIn: false }));
+  }, [setState]);
   return <Navigate to="/login" />;
 }
 
