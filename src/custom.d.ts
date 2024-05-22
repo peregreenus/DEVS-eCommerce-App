26582cbd1aa366250042abb6a1e43fe64df1d393
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-22 18:34:55 +0300

    declare modules for images

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.module.css';
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.gif' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: any;
+  export = value;
+}
