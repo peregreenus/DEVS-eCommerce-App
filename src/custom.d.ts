f0805b46a77bfbabe813b8ad629d85e2106ecf6a
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-05-22 18:48:22 +0300

    chore: add modules for images

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,27 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.module.css';
-
-declare module '*.png' {
-  const value: any;
-  export = value;
-}
-
-declare module '*.jpg' {
-  const value: any;
-  export = value;
-}
-
-declare module '*.jpeg' {
-  const value: any;
-  export = value;
-}
-
-declare module '*.gif' {
-  const value: any;
-  export = value;
-}
-
-declare module '*.svg' {
-  const value: any;
-  export = value;
-}
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
