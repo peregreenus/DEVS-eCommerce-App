7cffa093b6bf4d10c632efbb7a19a093479b51a1
Zelenskiyas <zelensk1973@gmail.com>
2024-05-26 09:38:56 +0300

    fix: remove as

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const div: HTMLElement = document.createElement('div');
+const div = document.createElement('div');
 div.setAttribute('id', 'root');
 document.body.append(div);
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+
+const rootElement = document.getElementById('root');
+if (rootElement !== null) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error('Root element not found');
+}
