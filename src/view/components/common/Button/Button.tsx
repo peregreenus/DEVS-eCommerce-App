5a5c87841bd82136eafa04dc94650db4e60bcb0a
Zelenskiy <Zelensk1973@gmail.com>
2024-05-30 09:58:45 +0300

    fix: naming error buttom component (#136)

--- src/view/components/common/Button/Button.tsx
+++ src/view/components/common/Button/Button.tsx
@@ -2,13 +2,13 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button: React.FC<ButtonProps> = function MyButton({ children, className, ...props }) {
+function Button({ children, className, ...props }: ButtonProps) {
   return (
     // eslint-disable-next-line
     <button className={className} {...props}>
       {children}
     </button>
   );
-};
+}
 
 export default Button;
