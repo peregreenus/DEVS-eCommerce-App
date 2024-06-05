242a919ecdfa8c18d67d1b344813983ea50e3ce2
Aliaksandr Andreichuk <70761678+peregreenus@users.noreply.github.com>
2024-06-05 14:32:25 +0300

    Refactor/catalog filter (#147)
    * refactor: prettier
    
    * refactor: prettier getProducts.ts
    
    * fix: NaN in empty filter fields
    
    * refactor: move filter component, increase maxPrice
    
    * refactor: change filter styles

--- src/data/api/getProducts.ts
+++ src/data/api/getProducts.ts
@@ -5,7 +5,6 @@ import { IProduct } from '../types/interfaces/product';
 import { MainProps } from '../types/main-props';
 import { getLSToken, getLSAnonToken } from '../utils/getLS';
 
-
 async function getProducts(
   { minPrice, maxPrice }: AppFilter,
   { state }: MainProps
@@ -15,14 +14,17 @@ async function getProducts(
   const token = getLSToken();
   const BEARER_TOKEN = token || getLSAnonToken();
   console.log(`token => ${BEARER_TOKEN}`, state);
+
   const params = new URLSearchParams();
   params.append('filter', `variants.price.centAmount:range (${minPrice} to ${maxPrice})`);
-  // eslint-disable-next-line no-console
+
   console.log(`variants.scopedPrice.currentValue.centAmount:range (${minPrice} to ${maxPrice})`);
+
   const headers = new Headers({
     Authorization: `Bearer ${BEARER_TOKEN}`
   });
   const fullUrl = `${url}?limit=42&${params.toString()}`;
+
   try {
     const response = await fetch(fullUrl, { method: 'GET', headers });
     const productsData = await response.json();
