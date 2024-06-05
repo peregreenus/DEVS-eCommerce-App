79f1e4d525dd43c536d17dff1c3df7dcae759c07
Zelenskiyas <zelensk1973@gmail.com>
2024-06-05 18:46:18 +0300

    feat: add style

--- src/view/components/common/Categories/categories.tsx
+++ src/view/components/common/Categories/categories.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import * as classes from './categories.module.css';
 import CategoriesProps from '../../../../data/types/interfaces/CategoriesProps';
 
-function Categories({ categories, setCategoryId }: CategoriesProps) {
+function Categories({ categories, categoryId, setCategoryId }: CategoriesProps) {
   const handleCategoryClick = (id: string) => {
     setCategoryId(id);
   };
@@ -12,13 +12,23 @@ function Categories({ categories, setCategoryId }: CategoriesProps) {
   return (
     <ul className={classes.list}>
       <li>
-        <button onClick={() => handleCategoryClick('')} className={classes.button}>
+        <button
+          onClick={() => handleCategoryClick('')}
+          className={
+            categoryId === '' ? `${classes.button} ${classes.active}` : `${classes.button}`
+          }>
           All categories
         </button>
       </li>
       {categories.map((category) => (
         <li key={category.id}>
-          <button onClick={() => handleCategoryClick(category.id)} className={classes.button}>
+          <button
+            onClick={() => handleCategoryClick(category.id)}
+            className={
+              categoryId === category.id
+                ? `${classes.button} ${classes.active}`
+                : `${classes.button}`
+            }>
             {category.metaTitle.en}
           </button>
         </li>
