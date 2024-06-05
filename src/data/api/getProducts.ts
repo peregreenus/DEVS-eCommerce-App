1ef9a4d43dd4594afd4ed94f3aba38e362a96d73
Aliaksandr Andreichuk <70761678+peregreenus@users.noreply.github.com>
2024-06-06 00:48:10 +0300

    Revert "Feat/filters on catalog page (#149)" (#150)
    This reverts commit 24a5680e17e75b1bbf5a67bfd1b5c78681a0d7a5.

--- src/data/api/getProducts.ts
+++ src/data/api/getProducts.ts
@@ -8,12 +8,9 @@ import { getLSToken, getLSAnonToken } from '../utils/getLS';
 async function getProducts(
   sortingType: string,
   { minPrice, maxPrice }: AppFilter,
-  categoryId: string,
   { state }: MainProps
 ): Promise<IProduct[] | null> {
   // const url = `${CTP.API_URL}${CTP.PROJECT_KEY}/product-projections?limit=42`;
-  console.log(categoryId);
-
   const url = `${CTP.API_URL}${CTP.PROJECT_KEY}/product-projections/search`;
   const token = getLSToken();
   const BEARER_TOKEN = token || getLSAnonToken();
@@ -22,9 +19,6 @@ async function getProducts(
   const params = new URLSearchParams();
   params.append('filter', `variants.price.centAmount:range (${minPrice} to ${maxPrice})`);
 
-  const paramsCat = new URLSearchParams();
-  paramsCat.append('filter', `categories.id:"${categoryId}"`);
-
   if (sortingType) params.append('sort', `${sortingType}`);
 
   console.log(`variants.scopedPrice.currentValue.centAmount:range (${minPrice} to ${maxPrice})`);
@@ -32,18 +26,16 @@ async function getProducts(
   const headers = new Headers({
     Authorization: `Bearer ${BEARER_TOKEN}`
   });
-  const fullUrl =
-    categoryId === ''
-      ? `${url}?limit=42&${params.toString()}`
-      : `${url}?limit=42&${paramsCat.toString()}&${params.toString()}`;
+  const fullUrl = `${url}?limit=42&${params.toString()}`;
+
   try {
     const response = await fetch(fullUrl, { method: 'GET', headers });
     const productsData = await response.json();
     return productsData.results;
   } catch (error) {
     console.error(error);
+    return null;
   }
-  return null;
 }
 
 export default getProducts;
--- src/data/types/interfaces/CategoriesProps.ts
+++
@@ -1,8 +0,0 @@
-import { ICategory } from './category';
-
-export default interface CategoriesProps {
-  categories: ICategory[];
-  setCategoryId: React.Dispatch<React.SetStateAction<string>>;
-  categoryId: string;
-  // eslint-disable-next-line semi
-}
