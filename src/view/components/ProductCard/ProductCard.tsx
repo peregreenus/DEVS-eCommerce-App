09ebf63c555fdefd6691003f1bb5b2d609c195e9
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-06-16 15:36:19 +0300

    feat: add functionality to the card button

--- src/view/components/ProductCard/ProductCard.tsx
+++ src/view/components/ProductCard/ProductCard.tsx
@@ -1,16 +1,28 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/no-danger */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProductCardProps } from '../../../data/types/interfaces/productCardProps';
 import * as classes from './ProductCard.module.css';
 import formatPrice from '../../../data/utils/formatPrice';
 import Button from '../common/Button/Button';
-// import AddToCart from '../../../data/api/Cart/AddToCart';
+import AddToCart from '../../../data/api/Cart/AddToCart';
+import productInCart from '../../../data/utils/productInCart';
 
 function ProductCard({ product, goToProduct }: ProductCardProps) {
   const discountedPrice = product.masterVariant.prices[0].discounted?.value.centAmount;
+  const [inCart, setInCart] = useState<boolean>(false);
+
+  useEffect(() => {
+    const isInCart = async () => {
+      setInCart(await productInCart(product));
+    };
+
+    isInCart();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [product.id]);
 
   return (
     <div id={product.id} className={classes.card} onClick={() => goToProduct(product.id)}>
@@ -41,8 +53,15 @@ function ProductCard({ product, goToProduct }: ProductCardProps) {
       ) : (
         ''
       )}
-      <Button className={classes.cardBtn} onClick={() => console.log()} />
-      {/* <button className={classes.cardBtn} type="button" aria-label="Add To Cart" disabled /> */}
+      <Button
+        disabled={inCart}
+        className={classes.cardBtn}
+        onClick={(e) => {
+          e.stopPropagation();
+          AddToCart(product);
+          setInCart(true);
+        }}
+      />
     </div>
   );
 }
