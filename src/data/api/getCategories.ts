23c558c70030638d8bd6c7345121f33a6dfc759e
Zelenskiy <Zelensk1973@gmail.com>
2024-06-16 13:25:17 +0300

    Feat/style catalog double slider etc (#188)
    * refactor: replace inline style to css
    
    * fix: style on price double slider
    
    * fix: style on filter
    
    * fix: style and refrash token error
    
    * fix: style noLogin message

--- src/data/api/getCategories.ts
+++ src/data/api/getCategories.ts
@@ -3,11 +3,16 @@ import CTP from '../types/ctp';
 import { ICategory } from '../types/interfaces/category';
 import { MainProps } from '../types/main-props';
 import { getLSAnonToken, getLSToken } from '../utils/getLS';
+import getAnonToken from './getToken';
+import refreshToken from './refreshToken';
 
 async function getCategories({ state }: MainProps): Promise<ICategory[] | null> {
   const url = `${CTP.API_URL}${CTP.PROJECT_KEY}/categories`;
   const token = getLSToken();
   const BEARER_TOKEN = token ? getLSToken() : getLSAnonToken();
+  if (!BEARER_TOKEN) {
+    await getAnonToken();
+  }
   console.log(`token => ${BEARER_TOKEN}`, state);
   const headers = new Headers({
     'Content-Type': 'application/json',
@@ -16,8 +21,11 @@ async function getCategories({ state }: MainProps): Promise<ICategory[] | null> 
 
   try {
     const response = await fetch(`${url}?limit=100`, { method: 'GET', headers });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    if (!response.ok && response.status === 401) {
+      // await getAnonToken();
+      if (BEARER_TOKEN) {
+        await refreshToken(BEARER_TOKEN);
+      }
     }
     const data = await response.json();
     console.log('getCategories', data);
