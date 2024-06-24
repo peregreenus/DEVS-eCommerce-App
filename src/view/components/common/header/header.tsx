738d542a35f4d5d55169e815981d156b12c73472
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-06-24 14:47:43 +0300

    refactor: header styles

--- src/view/components/common/header/header.tsx
+++ src/view/components/common/header/header.tsx
@@ -4,14 +4,17 @@ import Navbar from '../navbar/navbar';
 import NologinMessage from '../NologinMessage/NologinMessage';
 import * as classes from './header.module.css';
 import { MainProps } from '../../../../data/types/main-props';
-import LogoSVG from '../../../../assets/icon/galactic-exclusive.svg';
+import Logo from '../../../../assets/img/project-logo.png';
 
 function Header({ state, setState }: MainProps) {
   return (
     <>
       <header className={classes.header}>
         <Link to="/">
-          <img src={LogoSVG} alt="rs school Logo" className={classes.logo} />
+          <img src={Logo} alt="Logo" className={classes.logo} />
+          <p>
+            GALACTIC <br /> EXCLUSIVE
+          </p>
         </Link>
         <Navbar state={state} setState={setState} />
       </header>
